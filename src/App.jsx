c1e21432ac09e8be4dd73763cbcccd7b20e7efc8
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-03-07 20:01:24 +0530

    start frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-red-300 w-full h-screen'></div>
+     <p className='text-xl'>Hello</p>
+    </>
+  )
+}
+
+export default App
