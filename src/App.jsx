44a654c1cb6d0b3b648603dbd883929e3b9dbdeb
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-03-08 19:07:35 +0530

    start practice for tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BsGraphDown} from 'react-icons/bs'
+import {BsBookmarkDash} from 'react-icons/bs'
+import { FaRegUser } from 'react-icons/fa'
+import { MdOutlineSpeaker } from 'react-icons/md'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,18 @@ function App() {
     <>
     <div className='w-full h-screen flex'>
       <div className='w-[300px] h-full bg-green-200'>
-
+         <button className='w-full h-[40px] text-[25px] font-bold bg-red-400 flex justify-center items-center ' >
+          <BsGraphDown/>
+          Dashboard</button>
+         <button className='w-full h-[40px] text-[25px] font-bold bg-red-400 flex justify-center items-center ' >
+          <BsBookmarkDash/>
+          Booking</button>
+         <button className='w-full h-[40px] text-[25px] font-bold bg-red-400  flex justify-center items-center' >
+          <MdOutlineSpeaker/>
+          Items</button>
+         <button className='w-full h-[40px] text-[25px] font-bold bg-red-400  flex justify-center items-center' >
+          <FaRegUser/>
+          Users</button>
       </div>
       <div className='w-full bg-red-900'>
         
