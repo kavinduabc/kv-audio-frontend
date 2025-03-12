27773331ad98333f9a97f70a087a4891631edfd2
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-03-12 05:52:24 +0530

    start loginnpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Admin from './pages/admin/Admin'
 import Home from './pages/home/Home'
+import Testing from './components/Testing'
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes path="/*">
+        <Route path='/testing' element={<Testing/>} />
+        <Route path='/login' element={<Login/>} />
         <Route path="/admin/*" element={<Admin />} />
         <Route path='/*' element={<Home/>} />
        
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import "./Login.css"
+
+
+export default function Login(){
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+     return(
+        <div className="bg-picture w-full h-screen flex justify-center items-center">
+           <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl flex justify-center
+           items-center flex-col relative ">
+             <img src="/logo.png " alt="logo" className="w-[100px] h-[100px] absolute top-1"/>
+             <input type="email" placeholder="Email" className="w-[300px] h-[50px]
+             bg-transparent border-b-2 border-white text-white text-2xl outline-none "/>
+
+             
+           </div>
+        </div>
+     )
+}
