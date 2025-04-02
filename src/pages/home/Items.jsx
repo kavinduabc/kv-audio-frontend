979d7e2c4a21c21e12e04bf13e491b4092386ef1
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-02 21:09:07 +0530

    backend connection successfully  in items page 🤞

--- src/pages/home/Items.jsx
+++ src/pages/home/Items.jsx
@@ -1,6 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
+
   export default function Items(){
+    
+    const [state,setState] = useState("loading")//loading,success,error
+    const [items,setItems] = useState([])
+
+    useEffect(()=>{
+    
+      //** feaching data in backend */
+      if(state == "loadong"){
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/product`).then((res)=>{
+          setItems(res.data)
+          setState("success")
+       }).catch((err)=>{
+          toast.error(err?.response?.data?.error||"An error occured")
+          setState("error")
+       })
+      }
+      
+    },[])
 
     return(
-        <div></div>
+        <div>
+          
+        </div>
     )
   }
