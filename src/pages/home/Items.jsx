e7a18a8d6c48c6fdd4992debb398f8bf9c1974aa
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-03 19:36:58 +0530

    items.jsx page ✔

--- src/pages/home/Items.jsx
+++ src/pages/home/Items.jsx
@@ -13,7 +13,7 @@ import toast from "react-hot-toast"
       if(state == "loadong"){
         axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/product`).then((res)=>{
           setItems(res.data)
-          setState("success")
+          //setState("success")
        }).catch((err)=>{
           toast.error(err?.response?.data?.error||"An error occured")
           setState("error")
@@ -23,8 +23,24 @@ import toast from "react-hot-toast"
     },[])
 
     return(
-        <div>
-          
+        <div className="w-full h-full flex flex-wrap justify-center pt-[50px]">
+           
+           {
+            //** start codding in rendaring method  */
+             state == "loading"&&<div className="w-full h-full b flex justify-center items-center">
+                 <div className="w-[50px] h-[50px] border-4 rounded-full border-t-green-500 animate-spin">
+
+                 </div>
+             </div>
+           } 
+           {
+            state == "success"&&
+            items.map((item)=>{
+              return(
+                <h1 key={item.key}>{item.name}</h1>
+              )
+            })
+           }
         </div>
     )
   }
