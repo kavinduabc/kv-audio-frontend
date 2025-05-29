f4fe12e7ab37e7b9e02422e71642bfc779bf3bc4
kavinduabc <piumalranepura2002@gmail.com>
2025-05-29 17:21:24 +0530

    start coding for gallery page

--- src/pages/home/Gallary.jsx
+++ src/pages/home/Gallary.jsx
@@ -1,5 +1,11 @@
 export default function Gallery(){
      return(
-        <div></div>
+        <div className="flex flex-col  h-full bg-gray-100"> 
+         <div className="flex  justify-between p-4 ">
+            <h1 className="">Gallery</h1>
+            <button>Add Review</button>
+         </div>
+
+        </div>
      )
 }
