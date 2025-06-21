8dd11a58a5e843d83e50123fe2c83ef6eb0692c7
kavinduabc <piumalranepura2002@gmail.com>
2025-06-21 16:23:07 +0530

    add more styels

--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -4,17 +4,17 @@ export default function ImageBox({ galery }) {
       <div className="relative">
         <div className="m-4 rounded-xl overflow-hidden">
           <img
-            src={galery.image}
+            src={galery.image || "c:/Users/USER/Pictures/Screenshots/Screenshot 2025-06-21 083235.png"}
             alt={galery.functionName}
-            className="w-full h-60 object-cover brightness-90 "
+            className="w-full h-60 object-cover brightness-90 border-4 border-gray-500 rounded-lg"
           />
         </div>
         <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-transparent pointer-events-none" />
       </div>
-      <div className="px-6  pb-4 text-white">
+      <div className="px-6 pb-4 text-white flex justify-between">
         <h4 className="text-lg font-semibold">{galery.functionName}</h4>
         <p className="text-sm text-gray-400">
-          {new Date(galery.date).toLocaleDateString()}
+          {galery.date ? new Date(galery.date).toLocaleDateString() : ""}
         </p>
       </div>
     </div>
