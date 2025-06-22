7ba9beb5b1dba0a5d363d4872fce8ae914805b8d
kavinduabc <piumalranepura2002@gmail.com>
2025-06-22 10:35:13 +0530

    start impelemet dashboard

--- src/pages/admin/AdminDashboad.jsx
+++ src/pages/admin/AdminDashboad.jsx
@@ -1,7 +1,76 @@
+import { FaBox, FaCog, FaShoppingCart, FaUser } from "react-icons/fa";
+import ADashCard from "../../components/ADashCard";
+import {Line,Bar} from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  LineElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 
 export default function AdminDashboard(){
+  
+
+   //duny data ...
+    const dataLine = {
+  labels: ['Jan', 'Feb', 'Mar'],
+  datasets: [
+    {
+      label: 'Sales',
+      data: [30, 40, 50],
+      borderColor: 'blue',
+      tension: 0.3,
+    },
+  ],
+};
+
+const dataBar = {
+  labels: ['Product A', 'Product B', 'Product C'],
+  datasets: [
+    {
+      label: 'Stock',
+      data: [100, 80, 60],
+      backgroundColor: 'orange',
+    },
+  ],
+};
+
     return(
-        <div>Hello world</div>
+        <div className="grow p-8">
+            <h2 className="text-2xl mb-4">Dashboard</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
+                <ADashCard icon={<FaShoppingCart/>} title="Orders" values="140" />
+                <ADashCard icon={<FaBox/>} title="Products" values="140" />
+                <ADashCard icon={<FaUser/>} title="Users" values="140" />
+                <ADashCard icon={<FaCog/>} title="Settings" values="140" />
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <div className="bg-white p-4 rounded-lg  shadow-md">
+                    <h3 className="text-lg font-semiblod mb-4">Sales Data</h3>
+                    <Line data={dataLine} />
+                </div>
+                <div className="bg-white p-4 rounded-lg  shadow-md">
+                    <h3 className="text-lg font-semiblod mb-4">Products Data</h3>
+                    <Bar data={dataBar} />
+                </div>
+            </div>
+        </div>
     )
 }
