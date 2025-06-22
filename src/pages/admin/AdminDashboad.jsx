b4338878d8be78b9b0f250344a3e3689b435324f
kavinduabc <piumalranepura2002@gmail.com>
2025-06-22 20:54:13 +0530

    fixd some errors

--- src/pages/admin/AdminDashboad.jsx
+++ src/pages/admin/AdminDashboad.jsx
@@ -1,6 +1,6 @@
 import { FaBox, FaCog, FaShoppingCart, FaUser } from "react-icons/fa";
 import ADashCard from "../../components/ADashCard";
-import {Line,Bar} from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
 import axios from "axios";
 import {
   Chart as ChartJS,
@@ -27,21 +27,22 @@ ChartJS.register(
   Legend
 );
 
-
-export default function AdminDashboard(){
-  //database connection and fecting data 
-  const [users,setUsers] = useState();
-  const [state,setState] = useState("Loading");
+export default function AdminDashboard() {
+  const [customerCount, setCustomerCount] = useState(0);
+  const [orderCount, setOrderCount] = useState(0);
+  const [monthlyOrders,setMonthlyOrders]=useState({labels:[],datasets:[]});
+  const [productCount, setProductCount] = useState(0);
+  const [state, setState] = useState("Loading");
 
   useEffect(()=>{
    if(state == "Loading"){
     const token = localStorage.getItem("token");
-    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/cuctomerCount`,{
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/customerCount`,{
         headers:{
             Authorization :`Bearer ${token}`,
         }
     }).then((res)=>{
-        setUsers(res.data);
+        setCustomerCount(res.data.customerCount);
         setState("success");
         console.log(res.data);
     }).catch((err)=>{
@@ -51,49 +52,83 @@ export default function AdminDashboard(){
    }
   },[])
 
-   //duny data ...
-    const dataLine = {
-  labels: ['Jan', 'Feb', 'Mar'],
-  datasets: [
-    {
-      label: 'Sales',
-      data: [30, 40, 50],
-      borderColor: 'blue',
-      tension: 0.3,
-    },
-  ],
-};
+  useEffect(()=>{
+    const token = localStorage.getItem("token");
 
-const dataBar = {
-  labels: ['Product A', 'Product B', 'Product C'],
-  datasets: [
-    {
-      label: 'Stock',
-      data: [100, 80, 60],
-      backgroundColor: 'orange',
-    },
-  ],
-};
+     axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/orders/orderCount`,{
+            headers:{
+                Authorization:`Bearer ${token}`,
+            },
+        }).then((res)=>{
+            setOrderCount(res.data.totalOrders);
+            setState("Success");
+            console.log("order count:", res.data.totalOrders);
+        }).catch((err)=>{
+            toast.error(err?.response?.data?.error || "Error fetch order count");
+            setState(err);
+        })
 
-    return(
-        <div className="grow p-8">
-            <h2 className="text-2xl mb-4">Dashboard</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
-                <ADashCard icon={<FaShoppingCart/>} title="Orders" values="140" />
-                <ADashCard icon={<FaBox/>} title="Products" values="140" />
-                <ADashCard icon={<FaUser/>} title="Users" values="140" />
-                <ADashCard icon={<FaCog/>} title="Settings" values="140" />
-            </div>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                <div className="bg-white p-4 rounded-lg  shadow-md">
-                    <h3 className="text-lg font-semiblod mb-4">Sales Data</h3>
-                    <Line data={dataLine} />
-                </div>
-                <div className="bg-white p-4 rounded-lg  shadow-md">
-                    <h3 className="text-lg font-semiblod mb-4">Products Data</h3>
-                    <Bar data={dataBar} />
-                </div>
-            </div>
+  },[])
+
+   useEffect(()=>{
+    const token = localStorage.getItem("token");
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/product/productCount`,{
+        headers:{
+            Authorization:`Bearer ${token}`
+        },
+    }).then((res)=>{
+        setProductCount(res.data.totalProducts);
+        console.log("Product count:",res.data.totalProducts)
+        setState("success");
+    }).catch((err)=>{
+        toast.error(err?.response?.data?.error || "Error fetchin the total product ");
+        setState(err);
+    })
+
+  },[])
+
+  const dataLine = {
+    labels: ["Jan", "Feb", "Mar"],
+    datasets: [
+      {
+        label: "Sales",
+        data: [30, 40, 50],
+        borderColor: "blue",
+        tension: 0.3,
+      },
+    ],
+  };
+
+  const dataBar = {
+    labels: ["Product A", "Product B", "Product C"],
+    datasets: [
+      {
+        label: "Stock",
+        data: [100, 80, 60],
+        backgroundColor: "orange",
+      },
+    ],
+  };
+
+  return (
+    <div className="grow p-8">
+      <h2 className="text-2xl mb-4">Dashboard</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
+        <ADashCard icon={<FaShoppingCart />} title="Orders" value={orderCount} />
+        <ADashCard icon={<FaBox />} title="Products" value={productCount} />
+        <ADashCard icon={<FaUser />} title="Users" value={customerCount} />
+        <ADashCard icon={<FaCog />} title="Settings" value="140" />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="bg-white p-4 rounded-lg shadow-md">
+          <h3 className="text-lg font-semibold mb-4">Oders Dependencies</h3>
+          <Line data={dataLine} />
         </div>
-    )
+        <div className="bg-white p-4 rounded-lg shadow-md">
+          <h3 className="text-lg font-semibold mb-4">Products Data</h3>
+          <Bar data={dataBar} />
+        </div>
+      </div>
+    </div>
+  );
 }
